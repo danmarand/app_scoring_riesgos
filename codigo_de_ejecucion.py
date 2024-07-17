376ead9276e8bae754d76ea48068c1b50b14b8b6
danmarand <danielmarinandreu.ds@gmail.com>
2024-07-17 19:08:33 +0200

    Update codigo_de_ejecucion.py

--- codigo_de_ejecucion.py
+++ codigo_de_ejecucion.py
@@ -16,35 +16,6 @@ from sklearn.preprocessing import FunctionTransformer
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
 
-
-#2.CARGA DATOS
-ruta_proyecto = 'C:\Users\DANI\Desktop\PYTHON DATA SCIENCE MASTERY\EstructuraDirectorio\03_MACHINE_LEARNING\08_CASOS\03_RIESGOS\02_Datos\02_Validacion'
-nombre_fichero_datos = 'validacion.csv'
-ruta_completa = ruta_proyecto + '\\' + nombre_fichero_datos
-df = pd.read_csv(ruta_completa, index_col='id_cliente').drop(columns='Unnamed: 0')
-
-
-#3.VARIABLES Y REGISTROS FINALES
-variables_finales = ['ingresos_verificados',
-                     'vivienda',
-                     'finalidad',
-                     'num_cuotas',
-                     'antigüedad_empleo',
-                     'rating',
-                     'ingresos',
-                     'dti',
-                     'num_lineas_credito',
-                     'porc_uso_revolving',
-                     'principal',
-                     'tipo_interes',
-                     'imp_cuota',
-                     'num_derogatorios'
-                  ]
-a_eliminar = df.loc[df.ingresos > 300000].index.values
-df = df[~df.index.isin(a_eliminar)]
-df = df[variables_finales]
-
-
 #4.FUNCIONES DE SOPORTE
 def calidad_datos(temp):
     temp['antigüedad_empleo'] = temp['antigüedad_empleo'].fillna('desconocido')
@@ -54,10 +25,9 @@ def calidad_datos(temp):
 
 def creacion_variables(df):
     temp = df.copy()
-    temp.vivienda = temp.vivienda.replace(['ANY','NONE','OTHER'],'MORTGAGE')
-    temp.finalidad = temp.finalidad.replace(['wedding','educational','renewable_energy'],'otros')
-    return(temp)
-
+    temp.vivienda = temp.vivienda.replace(['ANY', 'NONE', 'OTHER'], 'MORTGAGE')
+    temp.finalidad = temp.finalidad.replace(['wedding', 'educational', 'renewable_energy'], 'otros')
+    return temp
 
 def ejecutar_modelos(df):
     #5.CALIDAD Y CREACION DE VARIABLES
@@ -65,31 +35,26 @@ def ejecutar_modelos(df):
     x_ead = creacion_variables(calidad_datos(df))
     x_lgd = creacion_variables(calidad_datos(df))
 
-
     #6.CARGA PIPES DE EJECUCION
     with open('pipe_ejecucion_pd.pickle', mode='rb') as file:
-       pipe_ejecucion_pd = pickle.load(file)
+        pipe_ejecucion_pd = pickle.load(file)
 
     with open('pipe_ejecucion_ead.pickle', mode='rb') as file:
-       pipe_ejecucion_ead = pickle.load(file)
+        pipe_ejecucion_ead = pickle.load(file)
 
     with open('pipe_ejecucion_lgd.pickle', mode='rb') as file:
-       pipe_ejecucion_lgd = pickle.load(file)
-
+        pipe_ejecucion_lgd = pickle.load(file)
 
     #7.EJECUCION
     scoring_pd = pipe_ejecucion_pd.predict_proba(x_pd)[:, 1]
     ead = pipe_ejecucion_ead.predict(x_ead)
     lgd = pipe_ejecucion_lgd.predict(x_lgd)
 
-
     #8.RESULTADO
     principal = x_pd.principal
-    EL = pd.DataFrame({'principal':principal,
-                     'pd':scoring_pd,
-                     'ead':ead,
-                     'lgd':lgd
-                     })
-    EL['perdida_esperada'] = round(EL.pd * EL.principal * EL.ead * EL.lgd,2)
-
-    return(EL)
+    EL = pd.DataFrame({'principal': principal,
+                       'pd': scoring_pd,
+                       'ead': ead,
+                       'lgd': lgd})
+    EL['perdida_esperada'] = round(EL.pd * EL.principal * EL.ead * EL.lgd, 2)
+    return EL
